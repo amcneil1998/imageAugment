6d2f041bdc720c109416426418e8345cce69a8f6
Jake Kenniv <jakenniv@cedarville.edu>
2019-11-02 22:15:57 -0400

    rotation and zoom

--- Augment.py
+++ Augment.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import imutils
+import os
+from random import randint
 
 #this method will perform all image agmentation on an image
 #input values can be either Booleans such as false or 
@@ -29,47 +31,46 @@ class Generator():
 
     def augment(self):
 
-        image = cv2.imread(self.imagePath)
+        for filename in os.listdir(self.imagePath):
+            image = cv2.imread(filename)
 
 
-        #do zoom
+            #do zoom
 
-        #do shear
+            #do shear
 
-        #flip only on horizontal
-        if self.doHorizontalFlips and not self.doVerticalFlips:
-            image = cv2.flip(image, 0)
+            #flip only on horizontal
+            if self.doHorizontalFlips and not self.doVerticalFlips:
+                image = cv2.flip(image, 0)
 
-        #flip only on verticle
-        if self.doVerticalFlips and not self.doHorizontalFlips:
-            image = cv2.flip(image, 1)
+            #flip only on verticle
+            if self.doVerticalFlips and not self.doHorizontalFlips:
+                image = cv2.flip(image, 1)
 
-        #flip both verticlly and horizontally
-        if self.doVerticalFlips and self.doHorizontalFlips:
-            image = cv2.flip(image, -1)
+            #flip both verticlly and horizontally
+            if self.doVerticalFlips and self.doHorizontalFlips:
+                image = cv2.flip(image, -1)
 
 
-        #add blur
-        if self.addBlur:
-            kernelSize = int(self.addBlur * 100)
-            if kernelSize % 2 == 0:
-                kernelSize += 1
-            image = cv2.GaussianBlur(image, (kernelSize, kernelSize), 0)
-        
-        #add rotation
-        if self.doRotation:
-            rotation = np.random.rand([0, self.doRotation])
-            angle = np.arange(rotation, self.doRotation)
-            rotated = imutils.rotate_bound(image, angle)
-            cv2.imshow("Rotated (Correct)", rotated)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            #add blur
+            if self.addBlur:
+                kernelSize = int(self.addBlur * 100)
+                if kernelSize % 2 == 0:
+                    kernelSize += 1
+                image = cv2.GaussianBlur(image, (kernelSize, kernelSize), 0)
+            
+            #add rotation
+            if self.doRotation:
+                rotation = randint(0,self.doRotation)
+                image = imutils.rotate_bound(image, rotation)
 
-        #change brightness
-        if self.augmentBrigtness:
-            cols, rows, none = image.shape
-            brightness = np.sum(image[:,:,-1])/(255*cols*rows)
-            image = cv2.convertScaleAbs(image, alpha=1, beta=(255*(1-brightness)))
+            #change brightness
+            if self.augmentBrigtness:
+                cols, rows, none = image.shape
+                brightness = np.sum(image[:,:,-1])/(255*cols*rows)
+                image = cv2.convertScaleAbs(image, alpha=1, beta=(255*(1-brightness)))
+
+            
         
 
     
