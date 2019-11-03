db695331ae0d84c39ffeea162721c60dff60998b
Jake Kenniv <jakenniv@cedarville.edu>
2019-11-02 21:25:57 -0400

    generator class

--- Augment.py
+++ Augment.py
@@ -1,29 +1,60 @@
 import cv2
-def augment(imagePath, Zoom=False, Shear=False, doHorizontalFlips=False, doVerticalFlips=False, augmentBrigtness=False, augmentSaturation=False, addBlur=False, addNoise=False):
-    image = cv2.imread(imagePath)
+import numpy as np
+import imutils
 
 
-    #do zoom
+class Generator():
+    def __init__(self, imagePath, zoom=False, shear=False, doHorizontalFlips=False, 
+            doVerticalFlips=False, augmentBrigtness=False, augmentSaturation=False, 
+            addBlur=False, addNoise=False, doRotation=False):
 
-    #do shear
-
-    #flip only on horizontal
-    if doHorizontalFlips and not doVerticalFlips:
-        image = cv2.flip(image, 0)
-
-    #flip only on verticle
-    if doVerticalFlips and not doHorizontalFlips:
-        image = cv2.flip(image, 1)
-
-    #flip both verticlly and horizontally
-    if doVerticalFlips and doHorizontalFlips:
-        image = cv2.flip(image, -1)
+            self.imagePath = imagePath
+            self.zoom = zoom
+            self.shear = shear
+            self.doHorizontalFlips = doHorizontalFlips
+            self.doVerticalFlips = doVerticalFlips
+            self.augmentBrigtness = augmentBrigtness
+            self.augmentSaturation = augmentSaturation
+            self.addBlur = addBlur
+            self.addNoise = addNoise
+            self.doRotation = doRotation
 
 
-    #add blur
-    if addBlur:
-        kernelSize = int(addBlur * 100)
-        if kernelSize % 2 == 0:
-            kernelSize += 1
-        image = cv2.GaussianBlur(image, (kernelSize, kernelSize), 0)
-    
+    def augment(self):
+
+        image = cv2.imread(self.imagePath)
+
+
+        #do zoom
+
+        #do shear
+
+        #flip only on horizontal
+        if self.doHorizontalFlips and not self.doVerticalFlips:
+            image = cv2.flip(image, 0)
+
+        #flip only on verticle
+        if self.doVerticalFlips and not self.doHorizontalFlips:
+            image = cv2.flip(image, 1)
+
+        #flip both verticlly and horizontally
+        if self.doVerticalFlips and self.doHorizontalFlips:
+            image = cv2.flip(image, -1)
+
+
+        #add blur
+        if self.addBlur:
+            kernelSize = int(self.addBlur * 100)
+            if kernelSize % 2 == 0:
+                kernelSize += 1
+            image = cv2.GaussianBlur(image, (kernelSize, kernelSize), 0)
+        
+        #add rotation
+        if self.doRotation:
+            rotation = np.random.rand([0, self.doRotation])
+            angle = np.arange(rotation, self.doRotation)
+            rotated = imutils.rotate_bound(image, angle)
+            cv2.imshow("Rotated (Correct)", rotated)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
