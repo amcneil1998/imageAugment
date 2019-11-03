d3eaac0a767692b1821a4a99bfd54df3ffa6628a
Jake Kenniv <jakenniv@cedarville.edu>
2019-11-02 22:57:33 -0400

    added zoom

--- Augment.py
+++ Augment.py
@@ -9,7 +9,7 @@ class Generator():
     #this will create a generator that yeilds images loaded from the directory specified
     #in dirPath.  Output will yeild images of size Batch_sizeximagecolsximagerowsximagedepth.
     #values specified are transfered directly into agument image
-    def createGenerator(dirPath, Batch_size, zoom=False, shear=False, doHorizontalFlips=False, doVerticalFlips=False, augmentBrigtness=False, augmentSaturation=False, addBlur=False, addNoise=False, doRotation=False):
+    def createGenerator(self, dirPath, Batch_size, zoom=False, shear=False, doHorizontalFlips=False, doVerticalFlips=False, augmentBrigtness=False, augmentSaturation=False, addBlur=False, addNoise=False, doRotation=False):
         nameList = os.listdir(dirPath)
         testImage = cv2.imread(nameList[0])
         cols, rows, depth = testImage.shape
@@ -30,10 +30,22 @@ class Generator():
                                 addNoise=addNoise,
                                 doRotation=doRotation)
     
-    #this method will take in an array of images as well as max agumentation values
+    #This method will take in an array of images as well as max agumentation values
     #it will then compute a random uniform augmentation between the specified agumentation value
-    #and zero. This will be done uniquely for each image.  The resulting array of agumented images is returned
-    def augmentImages(Images, zoom=False, shear=False, doHorizontalFlips=False, doVerticalFlips=False, augmentBrigtness=False, augmentSaturation=False, addBlur=False, addNoise=False, doRotation=False):
+    #and zero. In the case of rotation, the maximum value is 360 and min is 0 in degrees. 
+    #This will be done uniquely for each image.  The resulting array of agumented images is returned
+    def augmentImages(self, 
+                      Images, 
+                      zoom=False, 
+                      shear=False, 
+                      doHorizontalFlips=False, 
+                      doVerticalFlips=False, 
+                      augmentBrigtness=False, 
+                      augmentSaturation=False, 
+                      addBlur=False, 
+                      addNoise=False, 
+                      doRotation=False):
+
         for i in range(0, Images.shape[0]):
             if zoom:
                 zoomVal = np.random.uniform(0, zoom)
@@ -49,6 +61,7 @@ class Generator():
                 noiseVal = np.random.uniform(0, addNoise)
             if doRotation:
                 rotateVal = np.random.uniform(0, doRotation)
+
             Images[i] = augment(Images[i], 
                                 Zoom=zoomVal, 
                                 Shear=shearVal, 
@@ -58,7 +71,7 @@ class Generator():
                                 augmentSaturation=satVal, 
                                 addBlur=blurVal, 
                                 addNoise=noiseVal, 
-                                addRotation=rotateVal)
+                                doRotation=rotateVal)
         return Images
 
     #this method will perform all image agmentation on an image
@@ -66,9 +79,28 @@ class Generator():
     #decemals representing a 0-1.0 scale change
     #ceterain values like doHorizontalFlips should always be booleans
     #whereas others such as blur and brightness dont make since to be booleans
-    def augment(image, Zoom=False, Shear=False, doHorizontalFlips=False, doVerticalFlips=False, augmentBrigtness=False, augmentSaturation=False, addBlur=False, addNoise=False, doRotation=False):
+    def augment(self, 
+                image, 
+                zoom=False, 
+                shear=False, 
+                doHorizontalFlips=False, 
+                doVerticalFlips=False, 
+                augmentBrigtness=False, 
+                augmentSaturation=False, 
+                addBlur=False, 
+                addNoise=False, 
+                doRotation=False):
 
         #do zoom
+        if zoom:
+            height, width, depth = image.shape
+            cropXStart = int(width/2 - (1-zoom)/2*width)
+            cropXEnd = int(width/2 + (1-zoom)/2*width)
+            cropYStart = int(height/2 - (1-zoom)/2*height)
+            cropYEnd = int(height/2 + (1-zoom)/2*height)
+
+            cropped = image[cropYStart:cropYEnd, cropXStart:cropXEnd]
+            image = cv2.resize(cropped, (image.shape[1], image.shape[0]))
 
         #do shear
 
@@ -94,14 +126,13 @@ class Generator():
         
         #add rotation
         if doRotation:
-            angle = randint(0, doRotation)
-            rotated = imutils.rotate_bound(image, angle)
+            image = imutils.rotate_bound(image, doRotation)
 
         #change brightness
         if augmentBrigtness:
             cols, rows, none = image.shape
             brightness = np.sum(image[:,:,-1])/(255*cols*rows)
-            image = cv2.convertScaleAbs(image, alpha=1, beta=(255*(1-brightness)))
+            image = cv2.convertScaleAbs(image, alpha=1, beta=(255*(augmentBrigtness-brightness)))
         
 
     
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Rotations
 Zoom
 Shear
+-   https://stackoverflow.com/questions/44425701/shear-an-image-without-cropping
 Horizontal Flips
 Brightness
 Saturation
