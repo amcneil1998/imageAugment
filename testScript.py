7b75fcb45b9389e198e194a54f31ca8fda72ad03
Jake Kenniv <jakenniv@cedarville.edu>
2019-11-07 22:58:11 -0500

    fixed rotation

--- testScript.py
+++ testScript.py
@@ -1,3 +1,3 @@
 import Augment
-testGen = Augment.Generator.createGenerator(dirPath='/Users/cap1a1n/SeniorDesign/augment/pictures/', Batch_size=176, zoom=0.2, doHorizontalFlips=False, doVerticalFlips=False, augmentBrightness=0.2, addBlur=0.2, 
+testGen = Augment.Generator.createGenerator(dirPath='/home/jakekenn97/Desktop/imageAugment/pictures/', Batch_size=2, zoom=0.1, doHorizontalFlips=True, doVerticalFlips=False, augmentBrightness=1, addBlur=0.2, 
 addNoise=0.1, doRotation=20)
