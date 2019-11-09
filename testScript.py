bab10514f2f3d3826ae5c1d2b38156a3f841a715
amcneil1998 <alexanderjmcneil@cedarville.edu>
2019-11-09 18:43:08 -0500

    further changes

--- testScript.py
+++ testScript.py
@@ -1,6 +1,8 @@
 import cv2
 import Augment
-testImages = Augment.Generator.testGenerator(dirPath='/home/cap1a1n/SeniorDesign/ImageAugment/pictures/', numImages=10, zoom=0.1, doHorizontalFlips=False, doVerticalFlips=False, augmentBrightness=0.1, addBlur=0.1, 
+testImages = Augment.Generator.testGenerator(dirPath='/home/cap1a1n/SeniorDesign/ImageAugment/picturescopy/', numImages=256, zoom=0.1, doHorizontalFlips=True, doVerticalFlips=True, augmentBrightness=0.1, addBlur=0.1, 
 addNoise=0.05, doRotation=20)
+'''
 for i in range(0, len(testImages)):
     cv2.imshow("img", testImages[i]); cv2.waitKey(0); cv2.destroyAllWindows()
+'''
