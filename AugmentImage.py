a5b10170d99be9f5bbe1215b791efa970d35f4ad
amcneil1998 <alexanderjmcneil@cedarville.edu>
2019-11-11 23:53:21 -0500

    cleaned up repo

--- AugmentImage.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import imutils
-import math
-import numpy as np
-
-image = cv2.imread('/home/jakekenn97/Desktop/imageAugment/pictures/image1.png')
-
--- testScript.py
+++
@@ -1,6 +0,0 @@
-import cv2
-import Augment
-testImages = Augment.Generator.testGenerator(dirPath='/home/cap1a1n/SeniorDesign/ImageAugment/pictures/', numImages=10, zoom=0.1, doHorizontalFlips=False, doVerticalFlips=False, augmentBrightness=0.1, addBlur=0.1, 
-addNoise=0.05, doRotation=20)
-for i in range(0, len(testImages)):
-    cv2.imshow("img", testImages[i]); cv2.waitKey(0); cv2.destroyAllWindows()
