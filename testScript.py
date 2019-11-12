aa3b876051cb016e53f1918e80e8e8043c84cd45
amcneil1998 <alexanderjmcneil@cedarville.edu>
2019-11-11 23:52:18 -0500

    cleaned up repo

--- testScript.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import Augment
-testImages = Augment.Generator.testGenerator(trainImagePath='/home/cap1a1n/SeniorDesign/ImageAugment/picturescopy/', truthImagePath='/home/cap1a1n/SeniorDesign/ImageAugment/picturescopy/', numImages=256, zoom=0.1, doHorizontalFlips=True, doVerticalFlips=True, augmentBrightness=0.1, addBlur=0.1, 
-addNoise=0.05, doRotation=20)
-'''
-for i in range(0, len(testImages)):
-    cv2.imshow("img", testImages[i]); cv2.waitKey(0); cv2.destroyAllWindows()
-'''
